0ea63f6ce5fddc2dd97598b84a507445fca8bbe7
Robert <robert.cicero@hotmail.com>
2020-12-31 17:15:53 +0000

    Updated bug in mysql scripts

--- setup-scripts/tables.sql
+++ setup-scripts/tables.sql
@@ -1,6 +1,24 @@
 use main;
 
-create table auditLogs
+CREATE TABLE IF NOT EXISTS auditLogs
 (
-    eventDate datetime not null
+    eventDate datetime not null,
+    eventStatus varchar(100) null,
+    eventAction varchar(100) null,
+    eventDescription varchar(100) null
+);
+
+
+CREATE TABLE IF NOT EXISTS tradingHistory
+(
+    eventDate datetime not null,
+    eventAction varchar(100) null,
+    dealID varchar(100) null,
+    dealReference varchar(100) null,
+    epic varchar(100) null,
+    level double not null,
+    size double not null,
+    direction varchar(100) null,
+    profit double not null,
+    targetPrice double not null
 );
